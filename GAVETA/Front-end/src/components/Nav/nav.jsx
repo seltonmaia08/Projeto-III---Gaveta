0ab3dd4bd55b4acd831bb967cbcdc14efa2cdaad
alnufc <alan.vas.work@gmail.com>
2026-05-09 18:29:15 -0300

    adicao paginas adm, com router

--- GAVETA/Front-end/src/components/Nav/nav.jsx
+++ GAVETA/Front-end/src/components/Nav/nav.jsx
@@ -3,20 +3,30 @@ import ButtonNav from "../Button-nav/button-nav";
 import './nav.css'
 
 const buttons = [
-    {id: 1, title: 'Mural', color: '#FDCD9D', colorText: '#313131', router: '/'},
-    {id: 2, title: 'Memória', color: '#5C766D', colorText: '#EDE9E6', router: '/memoria'},
-    {id: 3, title: 'Pontos turísticos', color: '#43251D', colorText: '#EDE9E6', router: '/ponto-turistico'},
-    {id: 4, title: 'Compartilhar memórias', color: '#7A6047', colorText: '#EDE9E6', router: '/compartilhar-memoria'},
+    {id: 1, title: 'Mural', color: '#FDCD9D', colorText: '#313131', router: '/', admin: false},
+    {id: 2, title: 'Memória', color: '#5C766D', colorText: '#EDE9E6', router: '/memoria', admin: false},
+    {id: 3, title: 'Pontos turísticos', color: '#43251D', colorText: '#EDE9E6', router: '/ponto-turistico', admin: false},
+    {id: 4, title: 'Compartilhar memórias', color: '#7A6047', colorText: '#EDE9E6', router: '/compartilhar-memoria', admin: false},
+
+    // admin
+    {id: 5, title: 'Postadas', color: '#5C766D', colorText: '#EDE9E6', router: '/postadasDashboard', admin: true},
+    {id: 6, title: 'Pendentes', color: '#5C766D', colorText: '#EDE9E6', router: '/pendentesDashboard', admin: true},
+    {id: 7, title: 'Denuncia', color: '#5C766D', colorText: '#EDE9E6', router: '/denunciaDashboard', admin: true}
 ]
 
-const Nav = () => {
 
-    const [activeIndex, setActiveIndex] = useState('mural')    
+const Nav = ({ admin = false }) => {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const filteredButtons = buttons.filter(
+        button => button.admin === admin
+    );
 
     return (
         <nav className="nav-content">
             {
-                buttons.map((button) =>
+                filteredButtons.map((button) =>
                     <ButtonNav
                         key={button.id}
                         title={button.title}
@@ -32,4 +42,4 @@ const Nav = () => {
     )
 }
 
-export default Nav
+export default Nav;
--- GAVETA/Front-end/src/routers/appRouter.jsx
+++ GAVETA/Front-end/src/routers/appRouter.jsx
@@ -5,6 +5,12 @@ import Mural from '../pages/Mural/mural'
 import PontoTuristico from '../pages/Pontos_turisticos/pontosTuristicos'
 import Memoria from '../pages/memorias/memorias'
 import CompartilharMemoria from '../pages/compartilhar_memoria/compartilharMemoria'
+
+// import das paginas do adm
+import PostadasDashboard from '../pages_adm/Postadas/postadas'
+import PendentesDashboard from '../pages_adm/Pendentes/pendentes'
+import DenunciaDashboard from '../pages_adm/Denuncia/denuncia'
+
 const AppRouter = () => {
     return (
         <Routes>
@@ -13,9 +19,60 @@ const AppRouter = () => {
                 <Route path='/memoria' element={<Memoria />} />
                 <Route path='/ponto-turistico' element={<PontoTuristico/>} />
                 <Route path='/compartilhar-memoria' element={<CompartilharMemoria />} />
+
+                {/* Rotas do administrador, depois mudar a forma de acesso */}
+                <Route path='/postadasDashboard' element={<PostadasDashboard />} />
+                <Route path='/pendentesDashboard' element={<PendentesDashboard />} />
+                <Route path='/denunciaDashboard' element={<DenunciaDashboard />} />
+                
             </Route>
         </Routes>
     )
 }
 
 export default AppRouter
+
+// import { Routes, Route } from "react-router-dom";
+
+// import MainLayout from "../layout/mainLayout";
+
+// import Mural from "../pages/Mural";
+// import Memoria from "../pages/Memoria";
+
+// import PostadasDashboard from "../pages/admin/PostadasDashboard";
+// import PendentesDashboard from "../pages/admin/PendentesDashboard";
+// import DenunciaDashboard from "../pages/admin/DenunciaDashboard";
+
+// const AppRouter = () => {
+//     return (
+//         <Routes>
+
+//             <Route element={<MainLayout />}>
+
+//                 {/* públicas */}
+//                 <Route path="/" element={<Mural />} />
+//                 <Route path="/memoria" element={<Memoria />} />
+
+//                 {/* admin */}
+//                 <Route
+//                     path="/postadasDashboard"
+//                     element={<PostadasDashboard />}
+//                 />
+
+//                 <Route
+//                     path="/pendentesDashboard"
+//                     element={<PendentesDashboard />}
+//                 />
+
+//                 <Route
+//                     path="/denunciaDashboard"
+//                     element={<DenunciaDashboard />}
+//                 />
+
+//             </Route>
+
+//         </Routes>
+//     );
+// };
+
+// export default AppRouter;
