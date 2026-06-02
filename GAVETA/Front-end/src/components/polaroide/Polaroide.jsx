295d363b868a38d0a0562078a1fe969435cfad21
seltonmaia08 <seltonmaia08@gmail.com>
2026-06-01 21:40:31 -0300

    Resolução de conflitos nas telas de visualização de pontos pontosTuristicos

--- GAVETA/Front-end/src/components/polaroide/Polaroide.jsx
+++ GAVETA/Front-end/src/components/polaroide/Polaroide.jsx
@@ -3,10 +3,15 @@ import { Link, useLocation } from 'react-router-dom';
 import './polaroide.css';
 
 const Polaroide = ({ imagem, titulo, rotation, onClick, id }) => {
-  const rotate = useMemo(() => Math.floor(Math.random() * 20) - 10, []);
   const [colorPolaroide, setColorPolaroide] = useState(null)
+  const [isPontoTuristico, setIsPontoTuristico] = useState(false)
+
   const location = useLocation()
 
+  const randomValue = Math.floor(Math.random() * 20) - 10
+  const rotate = useMemo(() => randomValue, []);
+
+  const distancePolaroide = useMemo(() => Math.abs(Math.floor(randomValue * .2)), [])
 
   useEffect(() => {
     const pathName = location.pathname.slice(1)
@@ -15,16 +20,28 @@ const Polaroide = ({ imagem, titulo, rotation, onClick, id }) => {
       setColorPolaroide(pathName)
       console.log(pathName)
     }
+
+    if(pathName == 'ponto-turistico'){
+      setIsPontoTuristico(true)
+    }
+
   }, [location])
 
   return (
     <Link
       to={`/visualizar-memoria/${id}`}
       className='polaroide-link'
+      style={{
+          "--rotation": rotation ? `${rotate}deg` : '0deg',
+        }}  
     >
       <div
+        id='polaroide'
         className={`polaroide-card ${colorPolaroide}`}
-        style={{ "--rotation": rotation ? `${rotate}deg` : '0deg' }}
+        style={{
+          "--rotation": rotation ? `${rotate}deg` : '0deg',
+          marginRight: isPontoTuristico ? `${distancePolaroide}rem` : undefined
+        }}
         onClick={onClick}
       >
         <div
