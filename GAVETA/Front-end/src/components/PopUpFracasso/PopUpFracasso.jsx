816e1fdb7d8b3699a158cdd45e91ffe6c9476da6
Samuel Moreno <samu.m.araujo@gmail.com>
2026-06-06 12:38:34 -0300

    ajuste de pop ups no formulário, ajuste de anexo da imagem

--- GAVETA/Front-end/src/components/PopUpFracasso/PopUpFracasso.jsx
+++ GAVETA/Front-end/src/components/PopUpFracasso/PopUpFracasso.jsx
@@ -1,23 +1,29 @@
 import "./PopUpFracasso.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function PopUpFracasso({ onFechar, mensagem }) {
+
+  const [saindo, setSaindo] = useState(false);
 
   useEffect(
     
     () => {
-        const timer = setTimeout(() => {
-        onFechar();
-        }, 3000);
 
-        return () => clearTimeout(timer);
+      const timerSaida = setTimeout(() => {setSaindo(true)}, 2500);
+      const timer = setTimeout(() => {onFechar()}, 3000);
+
+      return () => {
+        clearTimeout(timer);
+        clearTimeout(timerSaida);
+      }
+
     }, [onFechar]
 )
 
   return (
 
-    <div className="container-fracasso">
-      <h2 className="titulo-fracasso">Falha!</h2>
+    <div className={`container-fracasso ${saindo ? "saindo" : ""}`}>
+      <h2 className="titulo-fracasso">Erro!</h2>
       <p className="texto-fracasso">{mensagem}</p>
     </div>
 
