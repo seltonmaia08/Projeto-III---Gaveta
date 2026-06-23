6d26ebb129b25a0b7b268bd3da7eea50850825cb
Samuel Moreno <samu.m.araujo@gmail.com>
2026-06-23 09:36:03 -0300

    leitura das denuncias e integração com info das postadas

--- GAVETA/Front-end/src/pages_adm/Denuncia/denuncia.jsx
+++ GAVETA/Front-end/src/pages_adm/Denuncia/denuncia.jsx
@@ -8,20 +8,21 @@ import PopUpConfirmacao from '../../components/PopUpConfirmacao/PopUpConfirmacao
 
 import './denuncia.css'
 
-import Dados from '../../services/dados.json'
-import { GetMemoriasDenunciadas } from '../../services/api';
+//import Dados from '../../services/dados.json'
+import { GetMemoriasDenunciadas, GetMemoriesByID } from '../../services/api';
 import FilterMemories from '../../components/filter-memories/FilterMemories';
+import { memo } from 'react';
 
 const DenunciaDashboard = () => {
   const [DenunciaInfoShow, setDenunciaInfoShow] = useState(false);
   const [confirmacaoAberta, setConfirmacaoAberta] = useState(false);
   const [sucessoAberto, setSucessoAberto] = useState(false);
 
-  const [exibirDados, setExibirDados] = useState(Dados) // substituir por [] e atualizar com método do useEffect()
+  const [exibirDados, setExibirDados] = useState([]);
   const [memoriaSelecionada, setMemoriaSelecionada] = useState(null);
 
-  const [openFilter, setOpenFilter] = useState(false)
-  const [filtrarConteudo, setFiltrarConteudo] = useState([])
+  const [openFilter, setOpenFilter] = useState(false);
+  const [filtrarConteudo, setFiltrarConteudo] = useState([]);
 
   useEffect(
 
@@ -30,16 +31,27 @@ const DenunciaDashboard = () => {
       async function Carregar() {
 
         const apiDenunciadas = await GetMemoriasDenunciadas();
-        console.log(apiDenunciadas);
-        //setExibirDados(apiDenunciadas); //descomentar isso depois
+
+        const memoriasCompletas = []; // aqui, vou juntar os atributos das postadas com o das denunciadas;
+        for(const denuncia of apiDenunciadas) { // percorro as denúncias;
+
+          const memoria = await GetMemoriesByID(denuncia.idMemoria); // pego a memória relacionada ao ID;
+          const memoriaCompleta = {...memoria, ...denuncia} // junto os atributos numa variável só com spread;
+          memoriasCompletas.push(memoriaCompleta); //coloco no vetor memoriasCompletas;
+        }
+
+        console.log(memoriasCompletas);
+        setExibirDados(memoriasCompletas);
       }
 
       Carregar();
     }, []
-  )
+  );
 
-  function handleDenunciaPopUp(e) {
+  function handleDenunciaPopUp(e, memoria) {
     e.preventDefault();
+
+    setMemoriaSelecionada(memoria);
     setDenunciaInfoShow(true);
   }
 
@@ -77,35 +89,34 @@ const DenunciaDashboard = () => {
         />
       </div>
       <div className="memorias-denuncias">
-        {exibirDados.map((memoria) => (
-          <Polaroide
-            key={memoria.id}
-            titulo={memoria.titulo}
-            imagem={memoria.imagem}
-            rotation={false}
-            onClick={handleDenunciaPopUp}
-          />
-        ))}
+        {exibirDados.map((memoria) => { 
+        return (
+          <>
+            <Polaroide
+              key={memoria.id}
+              titulo={memoria.titulo}
+              imagem={memoria.imagensURL}
+              rotation={false}
+              onClick={(event) => handleDenunciaPopUp(event, memoria)}
+            />
+          </>
+        )})}
       </div>
 
-      {DenunciaInfoShow && (
+      {DenunciaInfoShow && memoriaSelecionada &&(
         <DenunciaPopUp
-          titulo="Trilha da Galinha Choca"
-          nome="Roberto Silva"
-          data="27/04/2025"
-          texto="Era uma tarde quente em Quixadá quando saímos para a trilha da Pedra da Galinha Choca. O sol parecia mais forte que o normal, refletindo nas rochas e deixando o ar seco, daqueles que fazem a garganta pedir água a cada poucos minutos. Éramos quatro amigos, cada um carregando uma mochila simples, celular no bolso e a falsa confiança de quem acredita conhecer bem o sertão.
-
-        No começo, a subida parecia fácil. Entre risadas e histórias antigas, alguém apontava formações rochosas que lembravam animais, rostos ou objetos. O vento surgia às vezes, breve, quebrando o calor. Mas quanto mais avançávamos, mais o silêncio tomava conta do lugar. A cidade ficava distante e o único som constante era o dos passos sobre pedras soltas.
-        
-        Foi perto do topo que encontramos algo estranho: uma pequena cruz de madeira fincada entre duas rochas, envelhecida pelo tempo, sem nome ou data. A madeira estava tão desgastada que parecia ter passado décadas enfrentando sol, chuva e ventania. Ao lado dela havia uma garrafa de vidro parcialmente enterrada na areia avermelhada. A curiosidade falou mais alto. Um dos meus amigos retirou a garrafa com cuidado e percebeu que havia um papel dobrado dentro. Depois de alguma dificuldade para abrir a tampa enferrujada, conseguimos retirar o bilhete. A mensagem era curta, escrita com uma letra irregular e já desbotada pelo tempo: “Nem toda memória quer ser lembrada.” Por alguns segundos ficamos em silêncio, encarando aquelas palavras. Tentamos imaginar quem teria escrito aquilo, por qual motivo e há quanto tempo o bilhete estava ali. Surgiram hipóteses de todo tipo: uma brincadeira de turistas, uma lenda local ou até alguma espécie de homenagem esquecida. Apesar das piadas que fazíamos para aliviar a tensão, havia algo naquele lugar que despertava um desconforto difícil de explicar.
-
-        Decidimos continuar a caminhada, mas o clima já não era o mesmo. O caminho parecia mais longo e o silêncio mais pesado, como se o sertão ao nosso redor tivesse mudado de comportamento. As conversas diminuíram e passamos a prestar atenção em detalhes que antes ignorávamos: marcas estranhas em algumas pedras, o som distante de algo que parecia um assobio levado pelo vento e a sensação constante de que não estávamos sozinhos. Quando finalmente alcançamos um ponto mais alto da trilha e olhamos para a imensidão de Quixadá, o cenário continuava tão bonito quanto antes, mas nenhum de nós conseguiu apreciar a vista da mesma forma. Havia uma estranha impressão de que aquele bilhete não tinha sido deixado para qualquer pessoa, mas para alguém que um dia pisaria exatamente naquele lugar. E, pela primeira vez desde que começamos a subida, tive a sensação de que a trilha não era apenas um caminho entre as pedras: era um lugar onde histórias antigas permaneciam vivas, escondidas entre as rochas, esperando por alguém disposto a encontrá-las."
-          tags={["conto", "turismo"]}
-          lugar="Açude do Cedro"
-          email="robertoss2016@gmail.com"
-          outroContato="(88) 99999-9999"
-          denuncia="Conteúdo Impróprio"
-          foto="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/80/20/ac/img-20190503-092801729.jpg?w=1000&h=1000&s=1"
+          titulo= {memoriaSelecionada.titulo}
+          nome= {memoriaSelecionada.nomeAutor}
+          titulo={memoriaSelecionada.titulo}
+          nome={memoriaSelecionada.nomeAutor}
+          data={memoriaSelecionada.dataMemoria}
+          texto={memoriaSelecionada.relatoMemoria}
+          tags={memoriaSelecionada.tags}
+          lugar={memoriaSelecionada.ponto_memoria}
+          email={memoriaSelecionada.contatoAutor[0]}
+          outroContato={memoriaSelecionada.contatoAutor[1]}
+          denuncia= {memoriaSelecionada.motivo}
+          foto={memoriaSelecionada.imagensURL}
           onFechar={handleFechar}
           onDelete={handleAceitarRecusar}
           onIgnore={handleAceitarRecusar}
