44a4e56e2716fc7beef3aab23cf52fffac1418d3
Samuel Moreno <samu.m.araujo@gmail.com>
2026-06-24 10:27:31 -0300

    animação do popup de sucesso

--- GAVETA/Front-end/src/components/PopUpSucesso/PopUpSucesso.jsx
+++ GAVETA/Front-end/src/components/PopUpSucesso/PopUpSucesso.jsx
@@ -1,17 +1,25 @@
 import "./PopUpSucesso.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function PopUpSucesso({ onFechar }) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      onFechar();
-    }, 3000);
 
-    return () => clearTimeout(timer);
-  }, [onFechar]);
+  const [saindo, setSaindo] = useState(false);
+
+  useEffect(() => {
+
+      const timerSaida = setTimeout(() => {setSaindo(true)}, 2500);
+      const timer = setTimeout(() => {onFechar()}, 3000);
+
+      return () => {
+        clearTimeout(timer);
+        clearTimeout(timerSaida);
+      }
+
+    }, [onFechar]
+  );
 
   return (
-    <div className="container-sucesso">
+    <div className={`container-sucesso ${saindo ? "saindo" : ""}`}>
       <h2 className="titulo-sucesso">Sucesso!</h2>
       <p className="texto-sucesso">Sua ação foi concluída com êxito!</p>
     </div>
