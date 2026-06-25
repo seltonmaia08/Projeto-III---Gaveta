fd41b15c486b6099d47e038c57531982ee5523d7
seltonmaia08 <seltonmaia08@gmail.com>
2026-06-25 13:36:49 -0300

    atualização do botão de logout e ajuste da rota

--- GAVETA/Front-end/src/routers/RotasProtegidas.jsx
+++ GAVETA/Front-end/src/routers/RotasProtegidas.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export const RotaProtegidaAdmin = ({ isAdmin }) => {
     if (!isAdmin) {
-        return <Navigate to="/" replace />;
+        return <Navigate to="/login" replace />;
     }
 
     return <Outlet />;
